011109c8a2f9fea7f057e18fdada912d8f996848
Emanuel Menezes <menezesemanuel007@gmail.com>
2024-03-11 17:28:24 -0300

    Update script.js

--- shop/tshirt/js/script.js
+++ shop/tshirt/js/script.js
@@ -1,3 +1,18 @@
 function verso1(){
     document.getElementById("img_1").src="../../img/img_peita1_verso.png";
 }
+function verso2(){
+    document.getElementById("img_2").src="../../img/img_peita2_verso.png";
+}
+function verso3(){
+    document.getElementById("img_3").src="../../img/img_peita3_verso.png";
+}
+function verso4(){
+    document.getElementById("img_4").src="../../img/img_peita4_verso.png";
+}
+function verso5(){
+    document.getElementById("img_5").src="../../img/img_peita5_verso.png";
+}
+function verso6(){
+    document.getElementById("img_6").src="../../img/img_peita6_verso.png";
+}
