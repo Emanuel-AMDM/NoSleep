224928938c46ff6622557d86b8b182dfe95befd8
Emanuel Menezes <menezesemanuel007@gmail.com>
2024-03-12 15:30:36 -0300

    Update script.js

--- shop/tshirt/js/script.js
+++ shop/tshirt/js/script.js
@@ -1,24 +1,60 @@
-function verso1(){
-    var img_atual = "../../img/img_peita1_verso.png"
-    var img_anterior = "../../img/img_peita1_frente.png"
+var imgElement1 = document.getElementById("img_1");
+var img1 = "../../img/img_peita1_frente.png";
+var img2 = "../../img/img_peita1_verso.png";
+var imgAtual1 = img1; // Começa com a primeira imagem
 
-    document.getElementById("img_1").scr = img_atual;
-    var aux = img_atual
-    img_atual = img_anterior
-    img_anterior = aux
+var imgElement2 = document.getElementById("img_2");
+var img3 = "../../img/img_peita2_frente.png";
+var img4 = "../../img/img_peita2_verso.png";
+var imgAtual2 = img3; // Começa com a primeira imagem
+
+var imgElement3 = document.getElementById("img_3");
+var img5 = "../../img/img_peita3_frente.png";
+var img6 = "../../img/img_peita3_verso.png";
+var imgAtual3 = img5; // Começa com a primeira imagem
+
+var imgElement4 = document.getElementById("img_4");
+var img7 = "../../img/img_peita4_frente.png";
+var img8 = "../../img/img_peita4_verso.png";
+var imgAtual4 = img7; // Começa com a primeira imagem
+
+//se a imgAtual for identica a img1, ele troca a variavel para a segunda imagem, se não ele faz contrario
+function alternarImagem1() {
+    if (imgAtual1 === img1) {
+        imgElement1.src = img2;
+        imgAtual1 = img2;
+    } else {
+        imgElement1.src = img1;
+        imgAtual1 = img1;
+    }
 }
-function verso2(){
-    document.getElementById("img_2").src="../../img/img_peita2_verso.png";
+
+function alternarImagem2() {
+    if (imgAtual2 === img3) {
+        imgElement2.src = img4;
+        imgAtual2 = img4;
+    } else {
+        imgElement2.src = img3;
+        imgAtual2 = img3;
+    }
 }
-function verso3(){
-    document.getElementById("img_3").src="../../img/img_peita3_verso.png";
+
+function alternarImagem3() {
+    if (imgAtual3 === img5) {
+        imgElement3.src = img6;
+        imgAtual3 = img6;
+    } else {
+        imgElement3.src = img5;
+        imgAtual3 = img5;
+    }
 }
-function verso4(){
-    document.getElementById("img_4").src="../../img/img_peita4_verso.png";
-}
-function verso5(){
-    document.getElementById("img_5").src="../../img/img_peita5_verso.png";
-}
-function verso6(){
-    document.getElementById("img_6").src="../../img/img_peita6_verso.png";
+
+function alternarImagem4() {
+    if (imgAtual4 === img7) {
+        imgElement4.src = img8;
+        imgAtual4 = img8;
+    } else {
+        imgElement4.src = img7;
+        imgAtual4 = img7;
+    }
 }
