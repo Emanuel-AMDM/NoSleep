1d2e61081543bd4ba7cf8c27a19b975332233497
Emanuel Menezes <menezesemanuel007@gmail.com>
2024-03-22 15:57:34 -0300

    Create update_client.php

---
+++ services/update_client.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once('../database/execute_query.php');
+
+function update_client($id, $name, $surname, $email, $telephone, $gender, $birthday, $password, $dt_created_at){
+
+    //coloca a data de hoje
+    $agora = date('Y-m-d');
+    $dt_updated_at = $agora;
+
+    //checa se email ja existe
+    $sql1 = "SELECT * FROM client WHERE email = '$email'";
+    $emails = execute_query($sql1);
+
+    foreach($emails as $content){
+        if($content['email'] === $email && $content['id'] != $id){
+            return exit('E-mail já cadastrado');
+        }
+    }
+
+
+    //insert sql
+    $sql2 = "UPDATE client SET name = '$name', surname = '$surname', email = '$email', telephone = '$telephone', gender = '$gender', birthday = '$birthday', password = '$password', dt_created_at = '$dt_created_at', dt_updated_at = '$dt_updated_at' WHERE id = $id";
+    execute_query($sql2);
+}
