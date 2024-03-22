d666ed2bb19bf9662520ea814d15d49c54f1f46c
Emanuel Menezes <menezesemanuel007@gmail.com>
2024-03-22 16:12:34 -0300

    Update update.php

--- routes/update.php
+++ routes/update.php
@@ -4,4 +4,4 @@ require_once('../services/update_client.php');
 
 update_client($_GET['id'], $_POST['name'], $_POST['surname'], $_POST['email'], $_POST['telephone'], $_POST['gender'], $_POST['birthday'], $_POST['password'], $_POST['dt_created_at']);
 
-//header('Location: ../login/index.html');
+header('Location: ../login/index.html');
