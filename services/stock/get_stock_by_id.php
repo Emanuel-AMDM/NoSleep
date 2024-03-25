11fd3e283598d0b7070bf294a1fa824b7e894804
Emanuel Menezes <menezesemanuel007@gmail.com>
2024-03-25 17:15:59 -0300

    Create get_stock_by_id.php

---
+++ services/stock/get_stock_by_id.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once('../../database/execute_query.php');
+
+function get_stock_by_id($id){
+
+    $sql = "SELECT * FROM stock WHERE id = $id";
+    $stock = execute_query($sql);
+
+    foreach($stock as $stock){
+        if($stock['id'] == $id){
+            return $stock;
+        }
+    }
+
+}
