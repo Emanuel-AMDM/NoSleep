b94bee9347912ad4a643f3b4ce2a12ad6289cf8f
Emanuel Menezes <menezesemanuel007@gmail.com>
2024-03-25 17:15:55 -0300

    Update list_entity.php

--- services/list_entity.php
+++ services/list_entity.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('../../../database/execute_query.php');
+require_once('../database/execute_query.php');
 
 function list_entity_client(){
     
@@ -8,4 +8,12 @@ function list_entity_client(){
     $client = execute_query($sql);
 
     return $client;
+}
+
+function list_entity_stock(){
+    
+    $sql = "SELECT * FROM stock";
+    $stock = execute_query($sql);
+
+    return $stock;
 }
