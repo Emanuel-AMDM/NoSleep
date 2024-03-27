8a976311a0d3ab5d754d8b27450c4129a7bcdfca
Emanuel Menezes <menezesemanuel007@gmail.com>
2024-03-27 15:32:45 -0300

    Create update.php

---
+++ routes/client/update.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once('../../services/update.php');
+
+update_client($_GET['id'], $_POST['name'], $_POST['surname'], $_POST['email'], $_POST['telephone'], $_POST['gender'], $_POST['birthday'], $_POST['password'], $_POST['dt_created_at']);
+
+header('Location: ../login/index.html');
