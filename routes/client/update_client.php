4e15bc70911b56c6924cad53661af16508c56326
Emanuel Menezes <menezesemanuel007@gmail.com>
2024-03-27 15:32:47 -0300

    Delete update_client.php

--- routes/client/update_client.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once('../../services/client/update_client.php');
-
-update_client($_GET['id'], $_POST['name'], $_POST['surname'], $_POST['email'], $_POST['telephone'], $_POST['gender'], $_POST['birthday'], $_POST['password'], $_POST['dt_created_at']);
-
-header('Location: ../login/index.html');
