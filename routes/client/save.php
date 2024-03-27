c60d2f29fff677f9f93160909250b63026228187
Emanuel Menezes <menezesemanuel007@gmail.com>
2024-03-27 15:32:42 -0300

    Create save.php

---
+++ routes/client/save.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once('../../services/create.php');
+
+create_client($_POST['name'], $_POST['surname'], $_POST['email'], $_POST['telephone'], $_POST['gender'], $_POST['birthday'], $_POST['password']);
+
+header('Location: ../login/index.html');
