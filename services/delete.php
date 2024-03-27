fd9fba327ced89b06bd091d807891f87482082ed
Emanuel Menezes <menezesemanuel007@gmail.com>
2024-03-27 15:33:27 -0300

    Create delete.php

---
+++ services/delete.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once('../database/execute_query.php');
+
+function delete_client($id){
+
+    $sql = "DELETE FROM client WHERE id = $id";
+    execute_query($sql);
+}
+
+function delete_employee($id){
+
+    $sql = "DELETE FROM employee WHERE id = $id";
+    execute_query($sql);
+}
+
+function delete_stock($id){
+
+    $sql = "DELETE FROM stock WHERE id = $id";
+    execute_query($sql);
+}
