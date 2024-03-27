c684ed91022f269834ed1a0c2e84d2f40a1e6616
Emanuel Menezes <menezesemanuel007@gmail.com>
2024-03-27 15:32:56 -0300

    Create save.php

---
+++ routes/stock/save.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once('../../services/create.php');
+
+create_stock($_POST['part_code'], $_POST['sector'], $_POST['type'], $_POST['subtype'], $_POST['material'], $_POST['line'], $_POST['color'], $_POST['details'], $_POST['size'], $_POST['employee'], $_POST['dt_register'], $_POST['value'], $_POST['picture__input']);
+
+header('Location: ../../stock/create/index.php');
