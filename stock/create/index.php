eb97bde0f125e504abbb11658014f5b865959baf
Emanuel Menezes <menezesemanuel007@gmail.com>
2024-03-27 23:06:38 -0300

    Update index.php

--- stock/create/index.php
+++ stock/create/index.php
@@ -28,15 +28,14 @@ foreach($cod as $cod){
             <ul>
                 <li><img src="../../img_logo/Destaques_07 - Menu.png" alt=""></li>
                 <div class="menu">
-                    <li><a href="../../shop/index.html">Shop</a></li>
+                    <li><a href="../../shop/index.php">Shop</a></li>
                     <li><a href="../../MANUAL-DA-IDENTIDADE-VISUAL.pdf">Lookbook</a></li>
                     <li><a href="../index.php">Stock</a></li>
-                    <li><a href="../../employee/index.html">Employee Registration</a></li>
-                    <li><a href="../../client/index.php">Clients list</a></li>
+                    <li><a href="../../employee/index.php">Employee</a></li>
+                    <li><a href="../../client/index.php">Clients</a></li>
                 </div>
                 <div class="menu_login">
                     <li><a href="">Emanuel Menezes</a></li>
-                    <li><img src="../img/user.png" alt=""></li>
                 </div>
             </ul>
         </div>
@@ -98,6 +97,13 @@ foreach($cod as $cod){
             <div class="stock_content_column">
                 <label for="">Tamanho</label>
                 <input type="text" name="size" id="">
+            </div>
+        </div>
+
+        <div class="stock_content_center">
+            <div class="stock_content_column">
+                <label for="">Quantidade de Peças</label>
+                <input type="number" name="qntd_part" id="">
             </div>
         </div>
 
