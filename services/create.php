52ef5f09d2cc821f4d151b1b01fb6f9c4b77be9b
Emanuel Menezes <menezesemanuel007@gmail.com>
2024-03-27 23:06:31 -0300

    Update create.php

--- services/create.php
+++ services/create.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('../database/execute_query.php');
+require_once('../../database/execute_query.php');
 
 function create_client($name, $surname, $email, $telephone, $gender, $birthday, $password){
 
@@ -88,7 +88,7 @@ function create_employee($name, $dt_birth, $cpf_cnpj, $rg_ie, $telephone, $email
     execute_query($sql1);
 }
 
-function create_stock($part_code, $sector, $type, $subtype, $material, $line, $color, $details, $size, $employee, $dt_register, $value, $picture__input){
+function create_stock($part_code, $sector, $type, $subtype, $material, $line, $color, $details, $size, $employee, $dt_register, $value, $picture__input, $qntd_part){
 
     //coloca a data de hoje
     $agora = date('Y-m-d');
@@ -162,7 +162,7 @@ function create_stock($part_code, $sector, $type, $subtype, $material, $line, $c
     }
 
     //insert sql
-    $sql1 = "INSERT INTO stock (part_code, sector, type, subtype, material, line, color, details, size, employee, dt_register, value, dt_created_at, dt_updated_at, picture) VALUES ('$part_code', '$sector', '$type', '$subtype', '$material', '$line', '$color', '$details', '$size', '$employee', '$dt_register', '$value', '$dt_created_at', '$dt_updated_at', '$picture__input')";
+    $sql1 = "INSERT INTO stock (part_code, sector, type, subtype, material, line, color, details, size, employee, dt_register, value, qntd_part, dt_created_at, dt_updated_at, picture) VALUES ('$part_code', '$sector', '$type', '$subtype', '$material', '$line', '$color', '$details', '$size', '$employee', '$dt_register', '$value', '$qntd_part', '$dt_created_at', '$dt_updated_at', '$picture__input')";
     execute_query($sql1);
 }
 
