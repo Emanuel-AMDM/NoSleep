cb742ab9b2abc17095b66c56ff1aa01090e364ef
Emanuel Menezes <menezesemanuel007@gmail.com>
2024-03-27 23:06:43 -0300

    Update save.php

--- routes/stock/save.php
+++ routes/stock/save.php
@@ -2,6 +2,6 @@
 
 require_once('../../services/create.php');
 
-create_stock($_POST['part_code'], $_POST['sector'], $_POST['type'], $_POST['subtype'], $_POST['material'], $_POST['line'], $_POST['color'], $_POST['details'], $_POST['size'], $_POST['employee'], $_POST['dt_register'], $_POST['value'], $_POST['picture__input']);
+create_stock($_POST['part_code'], $_POST['sector'], $_POST['type'], $_POST['subtype'], $_POST['material'], $_POST['line'], $_POST['color'], $_POST['details'], $_POST['size'], $_POST['employee'], $_POST['dt_register'], $_POST['value'], $_POST['picture__input'], $_POST['qntd_part']);
 
 header('Location: ../../stock/create/index.php');
