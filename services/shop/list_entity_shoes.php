86a131771d0a1d4c3e6f88625f6a846e979c23be
Emanuel Menezes <menezesemanuel007@gmail.com>
2024-04-01 17:24:21 -0300

    Create list_entity_shoes.php

---
+++ services/shop/list_entity_shoes.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once('../../database/execute_query.php');
+
+function list_entity_stock(){
+    
+    $sql = "SELECT * FROM stock ORDER BY part_code DESC";
+    $stock = execute_query($sql);
+
+    return $stock;
+}
