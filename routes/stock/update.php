a8f762aec4707f0f6be7ab516ca075fc99d46b1c
Emanuel Menezes <menezesemanuel007@gmail.com>
2024-04-01 11:28:59 -0300

    Update update.php

--- routes/stock/update.php
+++ routes/stock/update.php
@@ -2,6 +2,11 @@
 
 require_once('../../services/update.php');
 
-update_stock($_GET['id'], $_POST['part_code'], $_POST['sector'], $_POST['type'], $_POST['subtype'], $_POST['material'], $_POST['line'], $_POST['color'], $_POST['details'], $_POST['size'], $_POST['employee'], $_POST['dt_register'], $_POST['value'], $_POST['dt_created_at'], $_POST['picture__input'], $_POST['qntd_part']);
+$name_picture = $_POST['picture__input'];
+if(isset($_FILES['picture__input'])){
+    move_uploaded_file($_FILES['picture__input']['tmp_name'], '../../uploads/' . $name_picture);
+}
+
+update_stock($_GET['id'], $_POST['part_code'], $_POST['sector'], $_POST['type'], $_POST['subtype'], $_POST['material'], $_POST['line'], $_POST['color'], $_POST['details'], $_POST['size'], $_POST['employee'], $_POST['dt_register'], $_POST['value'], $_POST['dt_created_at'], $name_picture, $_POST['qntd_part']);
 
 header('Location: ../../stock/index.php');
