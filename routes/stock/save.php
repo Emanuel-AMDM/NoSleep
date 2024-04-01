3cdc816cd27bcd1c3398aae8aa696a5bc0a32587
Emanuel Menezes <menezesemanuel007@gmail.com>
2024-04-01 11:28:57 -0300

    Update save.php

--- routes/stock/save.php
+++ routes/stock/save.php
@@ -2,6 +2,11 @@
 
 require_once('../../services/create.php');
 
-create_stock($_POST['part_code'], $_POST['sector'], $_POST['type'], $_POST['subtype'], $_POST['material'], $_POST['line'], $_POST['color'], $_POST['details'], $_POST['size'], $_POST['employee'], $_POST['dt_register'], $_POST['value'], $_POST['picture__input'], $_POST['qntd_part']);
+$name_picture = md5(uniqid(time() * rand())) . '.jpeg';
+if(isset($_FILES['picture__input'])){
+    move_uploaded_file($_FILES['picture__input']['tmp_name'], '../../uploads/' . $name_picture);
+}
+
+create_stock($_POST['part_code'], $_POST['sector'], $_POST['type'], $_POST['subtype'], $_POST['material'], $_POST['line'], $_POST['color'], $_POST['details'], $_POST['size'], $_POST['employee'], $_POST['dt_register'], $_POST['value'], $name_picture, $_POST['qntd_part']);
 
 header('Location: ../../stock/create/index.php');
