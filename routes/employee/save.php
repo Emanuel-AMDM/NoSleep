81c35bd0335e8e74d5851feb77529843453ef634
Emanuel Menezes <menezesemanuel007@gmail.com>
2024-04-02 15:24:35 -0300

    Update save.php

--- routes/employee/save.php
+++ routes/employee/save.php
@@ -2,6 +2,22 @@
 
 require_once('../../services/create.php');
 
-create_employee($_POST['name'], $_POST['dt_birth'], $_POST['cpf_cnpj'], $_POST['rg_ie'], $_POST['telephone'], $_POST['email'], $_POST['cep'], $_POST['road'], $_POST['neighborhood'], $_POST['city'], $_POST['state'], $_POST['complement'], $_POST['number'], $_POST['login'], $_POST['password']);
+create_employee(
+    mb_strtoupper($_POST['name']),
+    $_POST['dt_birth'],
+    $_POST['cpf_cnpj'],
+    $_POST['rg_ie'],
+    $_POST['telephone'],
+    $_POST['email'],
+    $_POST['cep'],
+    mb_strtoupper($_POST['road']),
+    mb_strtoupper($_POST['neighborhood']),
+    mb_strtoupper($_POST['city']),
+    mb_strtoupper($_POST['state']),
+    mb_strtoupper($_POST['complement']),
+    $_POST['number'],
+    $_POST['login'],
+    $_POST['password']
+);
 
 header('Location: ../../employee/create/index.php');
