b37b9158b386c9d2bdaf8b2ef878af1694995773
Emanuel Menezes <menezesemanuel007@gmail.com>
2024-04-02 15:24:34 -0300

    Update update.php

--- routes/employee/update.php
+++ routes/employee/update.php
@@ -2,6 +2,23 @@
 
 require_once('../../services/update.php');
 
-update_employee($_GET['id'], $_POST['name'], $_POST['dt_birth'], $_POST['cpf_cnpj'], $_POST['rg_ie'], $_POST['telephone'], $_POST['email'], $_POST['cep'], $_POST['road'], $_POST['neighborhood'], $_POST['city'], $_POST['state'], $_POST['complement'], $_POST['number'], $_POST['login'], $_POST['password'], $_POST['dt_created_at']);
+update_employee(
+    $_GET['id'],
+    mb_strtoupper($_POST['name']),
+    $_POST['dt_birth'],
+    $_POST['cpf_cnpj'],
+    $_POST['rg_ie'],
+    $_POST['telephone'],
+    $_POST['email'],
+    $_POST['cep'],
+    mb_strtoupper($_POST['road']),
+    mb_strtoupper($_POST['neighborhood']),
+    mb_strtoupper($_POST['city']),
+    mb_strtoupper($_POST['state']),
+    mb_strtoupper($_POST['complement']),
+    $_POST['number'],
+    $_POST['login'],
+    $_POST['password'],
+    $_POST['dt_created_at']);
 
 header('Location: ../../employee/index.php');
