0fbedb0cbb8b04fed9b255896dee40b08d102f70
Emanuel Menezes <menezesemanuel007@gmail.com>
2024-04-02 15:24:44 -0300

    Update index.php

--- employee/edit/index.php
+++ employee/edit/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once('../../services/get_by_id.php');
+require_once('../../services/employee/get_by_id.php');
 
 $id = $_GET['id'];
 
@@ -20,21 +20,22 @@ $employee = get_employee_by_id($id);
 <body>
     
     <nav class="nav_menu">
-        <div>
+        <div class="nav_flex">
+            <div>
+                <a href="../../index/index.html"><img src="../../img_logo/Destaques_07 - Menu.png" alt=""></a>
+            </div>
             <ul>
-                <li><img src="../../img_logo/Destaques_07 - Menu.png" alt=""></li>
                 <div class="menu">
-                    <li><a href="../shop/index.html">Shop</a></li>
+                    <li><a href="../shop/index.php">Shop</a></li>
                     <li><a href="../MANUAL-DA-IDENTIDADE-VISUAL.pdf">Lookbook</a></li>
-                    <li><a href="../stock/index.php">Stock</a></li>
+                    <li><a href="../../stock/index.php">Stock</a></li>
                     <li><a href="../index.php">Employee</a></li>
-                    <li><a href="../client/index.php">Clients</a></li>
-                </div>
-                <div class="menu_login">
-                    <li><a href="">Emanuel Menezes</a></li>
-                    <li><img src="../../img/user.png" alt=""></li>
+                    <li><a href="../../client/index.php">Clients</a></li>
                 </div>
             </ul>
+            <div class="menu_login">
+                <li><a href="">Emanuel Menezes</a></li>
+            </div>
         </div>
     </nav>
 
@@ -125,7 +126,9 @@ $employee = get_employee_by_id($id);
         </div>
 
         <div class="employee_button_save">
-            <button type="submit">Save</button>
+            <div class="glow-on-hover">
+                <button type="submit">Save</button>
+            </div>
         </div>
     </form>
 
