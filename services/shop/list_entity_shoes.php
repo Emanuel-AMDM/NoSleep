857114e58cf29ea93d9267975be711b4d5c329dc
Emanuel Menezes <menezesemanuel007@gmail.com>
2024-04-02 16:11:56 -0300

    Update list_entity_shoes.php

--- services/shop/list_entity_shoes.php
+++ services/shop/list_entity_shoes.php
@@ -2,10 +2,10 @@
 
 require_once('../../database/execute_query.php');
 
-function list_entity_stock(){
+function list_entity_shoes(){
     
-    $sql = "SELECT * FROM stock ORDER BY part_code DESC";
-    $stock = execute_query($sql);
+    $sql = "SELECT * FROM stock WHERE line = 'nosleepedshoes'";
+    $shoes = execute_query($sql);
 
-    return $stock;
+    return $shoes;
 }
