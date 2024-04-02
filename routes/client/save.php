8da6f3d80a14478406d819772f2c52026c7b31b3
Emanuel Menezes <menezesemanuel007@gmail.com>
2024-04-02 15:24:38 -0300

    Update save.php

--- routes/client/save.php
+++ routes/client/save.php
@@ -2,6 +2,14 @@
 
 require_once('../../services/create.php');
 
-create_client($_POST['name'], $_POST['surname'], $_POST['email'], $_POST['telephone'], $_POST['gender'], $_POST['birthday'], $_POST['password']);
+create_client(
+    mb_strtoupper($_POST['name']),
+    mb_strtoupper($_POST['surname']),
+    $_POST['email'],
+    $_POST['telephone'],
+    $_POST['gender'],
+    $_POST['birthday'],
+    $_POST['password']
+);
 
-header('Location: ../login/index.php');
+header('Location: ../../../login/index.html');
