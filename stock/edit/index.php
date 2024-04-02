d53c8716b911c25fa2b63621899943fef5c89530
Emanuel Menezes <menezesemanuel007@gmail.com>
2024-04-02 15:24:27 -0300

    Update index.php

--- stock/edit/index.php
+++ stock/edit/index.php
@@ -21,20 +21,22 @@ $stock = get_stock_by_id($id);
 <body>
     
     <nav class="nav_menu">
-        <div>
+        <div class="nav_flex">
+            <div>
+                <a href="../../index/index.html"><img src="../../img_logo/Destaques_07 - Menu.png" alt=""></a>
+            </div>
             <ul>
-                <li><img src="../../img_logo/Destaques_07 - Menu.png" alt=""></li>
                 <div class="menu">
-                    <li><a href="../../shop/index.php">Shop</a></li>
-                    <li><a href="../../MANUAL-DA-IDENTIDADE-VISUAL.pdf">Lookbook</a></li>
-                    <li><a href="../index.php">Stock</a></li>
+                    <li><a href="../shop/index.php">Shop</a></li>
+                    <li><a href="../MANUAL-DA-IDENTIDADE-VISUAL.pdf">Lookbook</a></li>
+                    <li><a href="index.php">Stock</a></li>
                     <li><a href="../../employee/index.php">Employee</a></li>
-                    <li><a href="../../client/index.php">Clients</a></li>
-                </div>
-                <div class="menu_login">
-                    <li><a href="">Emanuel Menezes</a></li>
+                    <li><a href="../client/index.php">Clients</a></li>
                 </div>
             </ul>
+            <div class="menu_login">
+                <li><a href="">Emanuel Menezes</a></li>
+            </div>
         </div>
     </nav>
 
